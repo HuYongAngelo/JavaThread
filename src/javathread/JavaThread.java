dc156e6dadc31f65afd6ec792c8fab4e1820468e
hu.yongan <hu.yongan@TAVE-PC20.OSDBSESTO.local>
2020-11-24 13:38:42 +0100

    Thread T1 and T2

--- src/javathread/JavaThread.java
+++ src/javathread/JavaThread.java
@@ -15,7 +15,7 @@ public class JavaThread {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Processo T1 = new Processo("T1");
+        Processo T2 = new Processo("T2");
     }
-    
 }
