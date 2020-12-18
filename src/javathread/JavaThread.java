cbd4f1748b58885eabb76c868f0e4c06b8d7dd06
Hu Yong An <Hu Yong An@Angelo>
2020-12-18 11:50:36 +0100

    Add code

--- src/javathread/JavaThread.java
+++ src/javathread/JavaThread.java
@@ -1,21 +1,48 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package javathread;
 
-/**
- *
- * @author hu.yongan
- */
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class JavaThread {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        Processo T1 = new Processo("T1");
-        Processo T2 = new Processo("T2");
+//        Processo T1 = new Processo("T1");
+//        Processo T2 = new Processo("T2");
+
+        Conto conto =new Conto(1000);
+        Sportello ATM1=new Sportello(conto,"mamma");
+        Sportello ATM2=new Sportello(conto,"papa");
+        Thread mamma=new Thread(ATM1);
+        Thread papa=new Thread(ATM2);
+        mamma.start();
+        papa.start();
+        int qualeThread;// 0 = mamma ; 1 = papà
+        int cheOperazione;// 0 = versa ; 1 = preleva
+        int quanto;
+        while(true){
+            qualeThread = (int) (Math.random()*2);
+            if(qualeThread==0){
+                System.out.println("\nthread: Mamma");
+            }else{
+                System.out.println("\nthread: Papà");
+            }
+
+            cheOperazione = (int)(Math.random()*2);
+            if(cheOperazione==0){
+                System.out.println("operazione: Versa");
+            }else{
+                System.out.println("operazione: Preleva");
+            }
+            
+            
+            quanto = (int)(Math.random()*501);
+            System.out.println("valore: "+quanto);
+            
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(JavaThread.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
     }
 }
